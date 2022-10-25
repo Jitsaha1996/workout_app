05353a453fa771d7919551e139778c78429b7650
souravdutta8012 <souravdutta2108@gmail.com>
2022-10-25 16:53:40 +0530

    changes

--- Frontend/src/Constant/workoutsCatalouges.ts
+++ Frontend/src/Constant/workoutsCatalouges.ts
@@ -1,125 +1,124 @@
- export const catalouges = [
-    {
-      _id: "1",
-      title: "Cardio",
-      content:
-        `Cardio exercise,
+export const catalouges = [
+  {
+    _id: "1",
+    title: "Cardio",
+    content:
+      `Cardio exercise,
          which is sometimes referred to as aerobic exercise, is any rhythmic activity 
         that raises your heart rate into your target heart rate zone.
          This is the zone where you burn the most fat and calories.`,
-      url: `https://olimpsport.com/media/mageplaza/blog/post/image//w/y/wyprobuj-5-najlepszych-cwiczen-cardio-na-silowni_5.jpg`
-    },
-    {
-        _id: "2",
-        title: "Push Up",
-        content:
-          `a conditioning exercise performed in a prone position by raising and lowering the body with the straightening
+    url: `https://olimpsport.com/media/mageplaza/blog/post/image//w/y/wyprobuj-5-najlepszych-cwiczen-cardio-na-silowni_5.jpg`
+  },
+  {
+    _id: "2",
+    title: "Push Up",
+    content:
+      `a conditioning exercise performed in a prone position by raising and lowering the body with the straightening
            and bending of the arms while keeping the back straight
             and supporting the body on the hands and toes`,
-        url: "https://www.athletico.com/wp-content/uploads/2020/04/stay-home-fitness-challenge-featured.jpg",
-      },
-      {
-        _id: "3",
-        title: "Situp",
-        content:
-          `Cardio exercise,
+    url: "https://post.greatist.com/wp-content/uploads/sites/2/2019/05/PERFECT-PUSHUP.gif",
+  },
+  {
+    _id: "3",
+    title: "Situp",
+    content:
+      `Cardio exercise,
            which is sometimes referred to as aerobic exercise, is any rhythmic activity 
           that raises your heart rate into your target heart rate zone.
            This is the zone where you burn the most fat and calories.`,
-        url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
-      },
-      {
-        _id: "4",
-        title: "Burpee",
-        content:
-          `Cardio exercise,
+    url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
+  },
+  {
+    _id: "4",
+    title: "Burpee",
+    content:
+      `Cardio exercise,
            which is sometimes referred to as aerobic exercise, is any rhythmic activity 
           that raises your heart rate into your target heart rate zone.
            This is the zone where you burn the most fat and calories.`,
-        url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
-      },
-      {
-        _id: "5",
-        title: "Plank",
-        content:
-          `Cardio exercise,
+    url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
+  },
+  {
+    _id: "5",
+    title: "Plank",
+    content:
+      `Cardio exercise,
            which is sometimes referred to as aerobic exercise, is any rhythmic activity 
           that raises your heart rate into your target heart rate zone.
            This is the zone where you burn the most fat and calories.`,
-        url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
-      },
-      {
-        _id: "6",
-        title: "Mountain Climber",
-        content:
-          `Cardio exercise,
+    url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
+  },
+  {
+    _id: "6",
+    title: "Mountain Climber",
+    content:
+      `Cardio exercise,
            which is sometimes referred to as aerobic exercise, is any rhythmic activity 
           that raises your heart rate into your target heart rate zone.
            This is the zone where you burn the most fat and calories.`,
-        url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
-      },
-      {
-        _id: "7",
-        title: "Jumping Jack",
-        content:
-          `Cardio exercise,
+    url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
+  },
+  {
+    _id: "7",
+    title: "Jumping Jack",
+    content:
+      `Cardio exercise,
            which is sometimes referred to as aerobic exercise, is any rhythmic activity 
           that raises your heart rate into your target heart rate zone.
            This is the zone where you burn the most fat and calories.`,
-        url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
-      },
-      {
-        _id: "8",
-        title: "Squat",
-        content:
-          `Cardio exercise,
+    url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
+  },
+  {
+    _id: "8",
+    title: "Squat",
+    content:
+      `Cardio exercise,
            which is sometimes referred to as aerobic exercise, is any rhythmic activity 
           that raises your heart rate into your target heart rate zone.
            This is the zone where you burn the most fat and calories.`,
-        url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
-      },
-      {
-        _id: "9",
-        title: "Lunge",
-        content:
-          `Cardio exercise,
+    url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
+  },
+  {
+    _id: "9",
+    title: "Lunge",
+    content:
+      `Cardio exercise,
            which is sometimes referred to as aerobic exercise, is any rhythmic activity 
           that raises your heart rate into your target heart rate zone.
            This is the zone where you burn the most fat and calories.`,
-        url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
-      },
-      {
-        _id: "10",
-        title: "BuGlute bridge",
-        content:
-          `Cardio exercise,
+    url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
+  },
+  {
+    _id: "10",
+    title: "BuGlute bridge",
+    content:
+      `Cardio exercise,
            which is sometimes referred to as aerobic exercise, is any rhythmic activity 
           that raises your heart rate into your target heart rate zone.
            This is the zone where you burn the most fat and calories.`,
-        url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
-      },
-      {
-        _id: "11",
-        title: "Slide Lunge",
-        content:
-          `Cardio exercise,
+    url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
+  },
+  {
+    _id: "11",
+    title: "Slide Lunge",
+    content:
+      `Cardio exercise,
            which is sometimes referred to as aerobic exercise, is any rhythmic activity 
           that raises your heart rate into your target heart rate zone.
            This is the zone where you burn the most fat and calories.`,
-        url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
-      },
-      {
-        _id: "12",
-        title: "Biceps Curl",
-        content:
-          `Cardio exercise,
+    url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
+  },
+  {
+    _id: "12",
+    title: "Biceps Curl",
+    content:
+      `Cardio exercise,
            which is sometimes referred to as aerobic exercise, is any rhythmic activity 
           that raises your heart rate into your target heart rate zone.
            This is the zone where you burn the most fat and calories.`,
-        url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
-      },
-    
-    
-  ];
-  
-  
+    url: "https://zurubunch.com/wp-content/uploads/2021/10/sit-up-bar-assistant-416x416.jpg",
+  },
+
+
+];
+
