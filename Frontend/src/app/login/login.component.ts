2cb2d01caf06294f18c399c46d946d1fc757c0a9
souravdutta8012 <souravdutta2108@gmail.com>
2022-10-25 13:50:37 +0530

    changes

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  title="Login"
-  constructor() { }
+  title = "Login"
+  registerForm: FormGroup;
+
+  constructor(private frmBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm = this.frmBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(3)]],
+    });
   }
 
+  createRegister(): any {
+    if (this.registerForm.invalid) {
+      return;
+    } else {
+      console.log(this.registerForm.getRawValue());
+    }
+  }
 }
