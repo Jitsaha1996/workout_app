a7a48efe779e76aa047315af627f70c0480b1513
jit saha <sahajit1996@gmail.com>
2022-10-26 16:28:03 +0530

    updated

--- Frontend/src/Constant/workoutsCatalouges.ts
+++ Frontend/src/Constant/workoutsCatalouges.ts
@@ -14,61 +14,70 @@ export const catalouges = [
   {
     _id: "3",
     title: "Sit Up",
-    content: ``,
+    content: `Situps are classic abdominal exercises done by lying on your back and lifting your torso. They use your body weight to strengthen and tone the core-stabilizing abdominal muscles.`,
     url: "https://hips.hearstapps.com/hmg-prod/images/workouts/2016/08/situptwisting-1472152218.gif",
   },
   {
     _id: "4",
     title: "Burpee",
-    content: ``,
+    content: `Despite the funny name, and perhaps not being as well-known as pushups or squats, burpees are a challenging exercise that work many of the major muscle groups in your body.
+    A burpee is essentially a two-part exercise: a pushup followed by a leap in the air.`,
     url: "https://media.tenor.com/u2-VJiigKCkAAAAC/exercise-jump.gif",
   },
   {
     _id: "5",
     title: "Plank",
-    content: ``,
+    content: `There are many benefits of planking as planks are an excellent abdominal and core exercise. To ensure you keep your core strong and stable, add the plank to your ab workout program. Keep reading to find out plank exercise benefits,
+     and different types of plank exercises along with instructions.`,
     url: "https://hips.hearstapps.com/hmg-prod/images/workouts/2016/03/plank-1457045584.gif",
   },
   {
     _id: "6",
     title: "Mountain Climber",
-    content: ``,
+    content: `That's the concept behind mountain climbers. Performed from a plank position, you'll alternate bringing one knee to your chest, then back out again, speeding up each time until you're "running" against the floor.
+     While the move sounds simple, mountain climbers exercise almost the entire body and raise your heart rate.`,
     url: "https://hips.hearstapps.com/hmg-prod/images/workouts/2016/08/mountainclimber-1472061303.gif?crop=1xw:1xh;center,top&resize=640:*",
   },
   {
     _id: "7",
     title: "Jumping Jack",
-    content: ``,
+    content: `Since jumping jacks only require your body weight, they're also a great cardiovascular exercise that you can do anywhere and anytime. In addition to increasing your heart rate and improving muscular strength and endurance, jumping jacks are also a fantastic way to burn calorie`,
     url: "https://www.icegif.com/wp-content/uploads/jumping-jacks-icegif-5.gif",
   },
   {
     _id: "8",
     title: "Squat",
-    content: ``,
+    content: `Squats are a total staple in butt and leg workouts. It just so happens that they can help sculpt the lower-body of your wildest dreams.`,
     url: "https://hips.hearstapps.com/ame-prod-menshealth-assets.s3.amazonaws.com/main/assets/bodyweight-squat.gif",
   },
   {
     _id: "9",
     title: "Lunge",
-    content: ``,
+    content: `Stand with your feet hip-width apart, keep your back straight, your shoulders back, and your abs tight.
+    Take a step forward and slowly bend both knees, until your back knee is just above the floor.
+    Stand back up and repeat the movement.
+    Alternate legs until the set is complete`,
     url: "https://media.tenor.com/fWiC9Ze5eUMAAAAC/lunges-exercise.gif",
   },
   {
     _id: "10",
     title: "Buglute Bridge",
-    content: ``,
+    content: `What does a glute bridge work?
+    Most specifically, glute bridges work the gluteus maximus. The hip extension aspect of this exercise involves the hamstrings, and the core aspect targets the transversus abdominis. When you do this exercise with a resistance band above your knees,
+     the gluteus medius portion of the glutes works extra hard`,
     url: "https://hips.hearstapps.com/hmg-prod/images/workouts/2016/08/glutebridgedynamic-1472218237.gif",
   },
   {
     _id: "11",
     title: "Slide Lunge",
-    content: ``,
+    content: `Bend your right leg while sliding the left foot backward into a lunge position. Keep the front knee behind the toe and keep the back leg slightly bent. Also keep the weight in the front leg so you always have control of the foot resting on the plate`,
     url: "https://media.tenor.com/DwuD868X1OUAAAAC/side-lunge.gif",
   },
   {
     _id: "12",
     title: "Biceps Curl",
-    content: ``,
+    content: `When strengthening your biceps, it's vital to perform exercises targeting the biceps muscle in your upper arm. Find different ways to work the biceps to keep your workouts challenging and exciting. These 10 bicep curl variations will help strengthen and tone your arm muscles. 
+    Incorporate some of them into your upper body strength training day, or use a couple as part of a full-body circuit`,
     url: "https://thumbs.gfycat.com/SlushyMetallicAtlanticsharpnosepuffer-size_restricted.gif",
   },
 ];
